0bb21fdee5e938a85a76477a45f43f57e5c944b7
Nebrass <lnibrass@gmail.com>
2015-05-01 00:35:02 +0200

    Adding voting labels + buttons + js methods for voting

--- app/js/Controllers/EventController.js
+++ app/js/Controllers/EventController.js
@@ -17,21 +17,34 @@ angular.module('eventsApp', []).controller('EventController',
                     name: 'AngularJS Get Started',
                     author: 'Scott Allen',
                     duration: '2h56m',
+                    upvoteCount: 0,
                     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.'
                 },
                 {
                     name: 'AngularJS Fundamentals',
                     author: 'Eames, Cooper',
                     duration: '6h19',
+                    upvoteCount: 0,
                     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.'
                 },
                 {
                     name: 'AngularJS Services in depth',
                     author: 'Brice Wilson',
                     duration: '3h3m',
+                    upvoteCount: 0,
                     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.'
                 }
             ]
         }
+
+        $scope.upvote = function (session) {
+            session.upvoteCount = session.upvoteCount + 1;
+        };
+
+        $scope.downvote = function (session) {
+            if (session.upvoteCount > 0) {
+                session.upvoteCount = session.upvoteCount - 1;
+            }
+        };
     }
 );
